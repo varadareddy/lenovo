ea2a89883fa9a3cb7c129ff487748f0422420a80
varadaReddy <varada.reddy866@gmail.com>
2022-06-07 16:35:58 +0000

    Lenovo Project files 1.0 version added

---
+++ src/main/java/ellucian/App.java
@@ -0,0 +1,13 @@
+package ellucian;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
